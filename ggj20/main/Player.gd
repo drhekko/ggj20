1fb3923e0a50096e4e38a390dfd2dc64174e17e0
drhekko <60531270+drhekko@users.noreply.github.com>
2020-02-01 16:08:02 -0400

    cambios8

--- ggj20/main/Player.gd
+++ ggj20/main/Player.gd
@@ -1,42 +1,38 @@
-extends Node2D
+extends KinematicBody2D
 
 export var speed = 300
-var screen_size  # Size of the game window.
-
-
 var velocity = Vector2()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
-	
-	
 func _process(delta):
-	var velocity = Vector2() #the player movement vector
+  velocity = Vector2()
+  if Input.is_action_pressed('ui_right'):
+    velocity.x += 1
+  if Input.is_action_pressed('ui_left'):
+    velocity.x -= 1
+  if Input.is_action_pressed('ui_down'):
+    velocity.y += 1
+  if Input.is_action_pressed('ui_up'):
+    velocity.y -= 1
+    
+  var movement = speed*velocity.normalized()*delta
+  self.move_and_collide(movement)
 
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-			
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	position += velocity * delta
-	
-	if Globalvar.item_selected != '':
-		#res://assets/abono.jpg
-		print('res://assets/'+Globalvar.item_selected+'.jpg')
-		$selected_object.texture = load('res://assets/'+Globalvar.item_selected+'.jpg')
+#func get_input():
+#    # Detect up/down/left/right keystate and only move when pressed.
+#    velocity = Vector2()
+#    if Input.is_action_pressed('ui_right'):
+#        velocity.x += 1
+#    if Input.is_action_pressed('ui_left'):
+#        velocity.x -= 1
+#    if Input.is_action_pressed('ui_down'):
+#        velocity.y += 1
+#    if Input.is_action_pressed('ui_up'):
+#        velocity.y -= 1
+#    velocity = velocity.normalized() * speed
+#
+#func _physics_process(delta):
+#    get_input()
+#    move_and_collide(velocity * delta)
 
 func player_dead():
-	print("Tas muelto")
+  print("Tas muelto")
