778f1175a49204727d96aaa2a0bd814d0e4af25a
drhekko <60531270+drhekko@users.noreply.github.com>
2020-02-01 18:40:30 -0400

    cambios9

--- ggj20/main/Abono.gd
+++ ggj20/main/Abono.gd
@@ -1,17 +1,29 @@
-extends Area2D
+extends RigidBody2D
+
+var picked = false
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var Globalvar
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		Globalvar = get_tree().get_root().get_node("/root/Globalvar")
+	pass
+
+func _physics_process(delta):
+	if picked==true:
+		self.position=get_node("../Player/Position2D").global_position
+		sleeping = true
+	else:
+		sleeping = false
 	
 func _input(event):
 	if Input.is_action_just_pressed("ui_select"):
-		var bodies = self.get_overlapping_bodies()
+		var bodies = $detector.get_overlapping_bodies()
 		for b in bodies:
-			if b.name == "Player":
-				Globalvar.item_selected = 'abono'
-				self.queue_free()
+			if b.name == "Player" and picked == false:
+				picked = true
+				print("Objeto tomado")
+	
+	if Input.is_action_just_pressed("ui_cancel") and picked == true:
+		picked = false
+		print("soltaste el objeto") 
--- ggj20/main/Player.gd
+++ ggj20/main/Player.gd
@@ -1,38 +1,34 @@
 extends KinematicBody2D
 
 export var speed = 300
+onready var sprite = get_node('AnimatedSprite')
 var velocity = Vector2()
 
 func _process(delta):
-  velocity = Vector2()
-  if Input.is_action_pressed('ui_right'):
-    velocity.x += 1
-  if Input.is_action_pressed('ui_left'):
-    velocity.x -= 1
-  if Input.is_action_pressed('ui_down'):
-    velocity.y += 1
-  if Input.is_action_pressed('ui_up'):
-    velocity.y -= 1
-    
-  var movement = speed*velocity.normalized()*delta
-  self.move_and_collide(movement)
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
 
-#func get_input():
-#    # Detect up/down/left/right keystate and only move when pressed.
-#    velocity = Vector2()
-#    if Input.is_action_pressed('ui_right'):
-#        velocity.x += 1
-#    if Input.is_action_pressed('ui_left'):
-#        velocity.x -= 1
-#    if Input.is_action_pressed('ui_down'):
-#        velocity.y += 1
-#    if Input.is_action_pressed('ui_up'):
-#        velocity.y -= 1
-#    velocity = velocity.normalized() * speed
-#
-#func _physics_process(delta):
-#    get_input()
-#    move_and_collide(velocity * delta)
+		self.sprite.flip_h = false
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+
+		self.sprite.flip_h = true
+	if Input.is_action_pressed('ui_down'):
+
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+
+		velocity.y -= 1
+		
+	if velocity.x != 0 || velocity.y != 0:
+		self.sprite.play('movimiento')
+	else:
+		self.sprite.play('reposo')
+	
+	var movement = speed*velocity.normalized()*delta
+	self.move_and_collide(movement)
+
 
 func player_dead():
   print("Tas muelto")
