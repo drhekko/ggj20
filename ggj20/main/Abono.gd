78d9025fb84ff2f54190946f19b99d98341f872c
drhekko <60531270+drhekko@users.noreply.github.com>
2020-02-01 14:42:23 -0400

    cambios 7

--- ggj20/main/Abono.gd
+++ ggj20/main/Abono.gd
@@ -3,15 +3,20 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var Globalvar
 # Called when the node enters the scene tree for the first time.
+var selected_item
+
 func _ready():
-		Globalvar = get_tree().get_root().get_node("/root/Globalvar")
+	selected_item = ''
+	pass
 	
 func _input(event):
+
 	if Input.is_action_just_pressed("ui_select"):
 		var bodies = self.get_overlapping_bodies()
 		for b in bodies:
 			if b.name == "Player":
-				Globalvar.item_selected = 'abono'
-				self.queue_free()
+				self.selected_item = 'abono'
+				self.position.x = 100
+				self.position.y = 100
+				#self.queue_free()
--- ggj20/main/Player.gd
+++ ggj20/main/Player.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 export var speed = 300
 var screen_size  # Size of the game window.
-
+var item_select = ''
 
 var velocity = Vector2()
 # Declare member variables here. Examples:
@@ -11,8 +11,9 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
-	
+
+	var PlayerReference = $Player
+
 	
 func _process(delta):
 	var velocity = Vector2() #the player movement vector
@@ -28,15 +29,14 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 			
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+
 	position += velocity * delta
 	
-	if Globalvar.item_selected != '':
+	if self.item_select != '':
 		#res://assets/abono.jpg
-		print('res://assets/'+Globalvar.item_selected+'.jpg')
-		$selected_object.texture = load('res://assets/'+Globalvar.item_selected+'.jpg')
+		if Input.is_action_just_pressed("ui_select"):
+			
+			$selected_object.texture = load('res://assets/'+self.item_select+'.jpg')
 
 func _on_Timer_timeout():
 	$Timer.stop()
