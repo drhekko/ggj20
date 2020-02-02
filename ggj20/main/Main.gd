e5620ef94c6c1eace2760f301f898438db563fcc
Rauljrs <60528325+Rauljrs@users.noreply.github.com>
2020-02-01 23:50:23 -0400

    evolucion flor

--- ggj20/main/Main.gd
+++ ggj20/main/Main.gd
@@ -1,4 +1,5 @@
 extends Node2D
+export (PackedScene) var flowy_2
 
 func _ready():
 	life()
@@ -24,3 +25,20 @@ func _on_1seg_timeout():
 	life()
 	if(int($Timer.time_left)==0):
 		$"1seg".stop()
+
+
+func _on_flowey_body_entered(body):
+	if $Abono.picked == true:
+		var bodies = $Abono/detector.get_overlapping_bodies()
+		for b in bodies:
+			if b.name == "Player" and $Abono.picked == true:
+				$Abono.picked = false
+				print("flow")
+				var newFlowy = get_tree().get_nodes_in_group("Main")[0].Flowy2.instance()
+				get_tree().get_nodes_in_group("Main")[0].add_child(newFlowy)
+				newFlowy.global.position = $flowey.global.position
+				$flowey.queue_free()
+				$Abono.queue_free()
+				print("Objeto pa la planta")
+	else:
+		print("no tienes nada")
